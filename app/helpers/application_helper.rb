9f29ac12709cedfce75597623fc3c822b3935360
Maximilian Richt <maxi@richt.name>
2016-02-22 19:16:08 +0100

    add search form to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def link_to_paper(title, paper, html_options = {})
     link_to title, paper_path(paper.department, paper), html_options
   end
+
+  def display_header_search?
+    !current_page?(root_path) && !current_page?(search_path)
+  end
 end

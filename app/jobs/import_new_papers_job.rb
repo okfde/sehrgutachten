72780af5bb0b409b78136af7ee46bbb4e43e8b73
Maximilian Richt <maxi@richt.name>
2016-02-22 18:20:45 +0100

    import or update papers

--- app/jobs/import_new_papers_job.rb
+++ app/jobs/import_new_papers_job.rb
@@ -5,15 +5,22 @@ class ImportNewPapersJob < ApplicationJob
 
     old_papers = 0
     new_papers = 0
-    papers.each do |paper|
-      if Paper.where(department: department, reference: paper[:reference], title: paper[:title]).exists?
+    papers.each do |item|
+      new_paper = false
+      if Paper.where(department: department, reference: item[:reference], title: item[:title]).exists?
+        paper = Paper.where(department: department, reference: item[:reference], title: item[:title]).first
         old_papers += 1
-        next
+        logger.info "[#{department.short_name}] Updating Paper: [#{item[:reference]}] \"#{item[:title]}\""
+        paper.assign_attributes(item.except(:reference))
+        paper.save!
+      else
+        logger.info "[#{department.short_name}] New Paper: [#{item[:reference]}] \"#{item[:title]}\""
+        item[:department] = department
+        paper = Paper.create(item)
+        new_papers += 1
       end
 
-      paper[:department] = department
-      Paper.create(paper)
-      new_papers += 1
+      DownloadPaperJob.perform_later(paper, force: new_paper) unless paper.url.blank?
     end
 
     logger.info "Importing done. #{new_papers} new Papers, #{old_papers} old Papers."

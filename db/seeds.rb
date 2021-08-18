b85b51855fda3e293594dac5efd582b3c85daf14
Malte Krupa <github-nqyroppg@nafn.de>
2021-08-18 14:13:32 +0200

    Add documentation for docker-compose setup
    and align docker-compose file with other okfde repos.
    
    Additionally, add some test data that is only applied to the development
    setup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  require 'time'
+
+  departments = [
+    ["wd1", "Geschichte, Zeitgeschichte und Politik", "http://www.bundestag.de/dokumente/analysen/wd1", "wd1"],
+    ["wd2", "Auswärtiges, Völkerrecht, wirtschaftliche Zusammenarbeit und Entwicklung, Verteidigung, Menschenrechte und humanitäre Hilfe", "http://www.bundestag.de/dokumente/analysen/wd2", "wd2"],
+    ["wd3", "Verfassung und Verwaltung", "http://www.bundestag.de/dokumente/analysen/wd3", "wd3"],
+    ["wd4", "Haushalt und Finanzen", "http://www.bundestag.de/dokumente/analysen/wd4", "wd4"],
+    ["wd5", "Wirtschaft und Verkehr, Ernährung und Landwirtschaft", "http://www.bundestag.de/dokumente/analysen/wd5", "wd5"],
+    ["wd6", "Arbeit und Soziales", "http://www.bundestag.de/dokumente/analysen/wd6", "wd6"],
+    ["wd7", "Zivil-, Straf- und Verfahrensrecht, Bau und Stadtentwicklung", "http://www.bundestag.de/dokumente/analysen/wd7", "wd7"],
+    ["wd8", "Umwelt, Naturschutz, Reaktorsicherheit, Bildung und Forschung", "http://www.bundestag.de/dokumente/analysen/wd8", "wd8"],
+    ["wd9", "Gesundheit, Familie, Senioren, Frauen und Jugend", "http://www.bundestag.de/dokumente/analysen/wd9", "wd9"],
+    ["wd10", "Kultur, Medien und Sport", "http://www.bundestag.de/dokumente/analysen/wd10", "wd10"],
+    ["pe6", "Europa", "http://www.bundestag.de/dokumente/analysen/pe6", "pe6"],
+  ]
+
+  departments.each do |short_name, subject, source_url, slug|
+    Department.create(short_name: short_name, subject: subject, source_url: source_url, slug: slug)
+  end
+
+  (1..25).each do |i|
+    Paper.create(
+      reference: "0#{rand(10..100)}/#{rand(100)}",
+      title: "Titel #{i}",
+      url: "https://example.com",
+      department_id: rand(1..departments.length()),
+      contents: ["TTIP", "Terrorismus"].sample,
+      page_count: rand(1..100),
+      downloaded_at: Time.now
+    )
+  end
+end
